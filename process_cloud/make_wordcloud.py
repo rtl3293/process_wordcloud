bfe22292876de8e27e394f4c638643ab2071d914
Ryan Lonergan <ryan.tlonergan@gmail.com>
2020-02-27 12:34:39 -0500

    Added make_wordcloud.py to begin transitioning from writing to a file to using the dictionary to create a wordcloud and display it using matplotlib

---
+++ process_cloud/make_wordcloud.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+import os
+import re
+from PIL import Image
+from os import path
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from get_rosters import get_rosters
+
+def makeImage():
+    #ricky_mask = np.array(Image.open("alice_mask.png"))
+    dict = get_rosters([2013, 2014, 2015, 2016])
+    wc = WordCloud()
+    # generate word cloud
+    wc.generate_from_frequencies(dict)
+
+    # show
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+
