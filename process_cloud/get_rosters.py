055ba335fb8adb5af9961d59e9eada904db0b6a1
Ryan Lonergan <ryan.tlonergan@gmail.com>
2020-02-27 16:46:05 -0500

    Working to use stats from selected season range and not use players career stats

--- process_cloud/get_rosters.py
+++ process_cloud/get_rosters.py
@@ -6,22 +6,25 @@ from sportsreference.nba.roster import Roster
 #from pprint import pprint
 #import csv
 #import os
-
+import pandas as pd
 #2013-2016
 def get_rosters(years):
     #years is a list of years to pull the rosters from
-#    with open("hinkie_roster.txt", "w") as f:
-        hinkie_roster = {}
+    hinkie_roster = {}
+    with open("hinkie_roster.txt", "w") as f:
         for year in years:
             #Gets roster for a given year
-            for player in Roster(team="PHI", year=str(year)).players:
+            for player in Roster(team="PHI", year="20" + year).players:
             #incrementing through the player info and formatting data
             #for wordcloud.generate_from_frequencies(hinkie_roster)
                 if player.name not in hinkie_roster:
-                    hinkie_roster[player.name] = player.games_played
-#        for name, gp in hinkie_roster.items():
-#            f.write("{}: Games Played: {}\n".format(name, gp))
-#    f.close()
-        return hinkie_roster
+                    hinkie_roster[player.name] = player.dataframe.loc["20" + year +"-{}".format(str(int(year) + 1))].games_played
+                else:
+                    hinkie_roster[player.name] += player.dataframe.loc["20" + year +"-{}".format(str(int(year) + 1))].games_played
+        for name, gp in hinkie_roster.items():
+                f.write("{}: Games Played: ".format(name, gp))
+    f.close()
+    return hinkie_roster
 
-#get_rosters([2013, 2014, 2015, 2016])
+get_rosters(["13", "14"])
+#, 2015, 2016]
