d6afabc4d938552ce5f4fb47582b5dd3cd9133d2
Ryan Lonergan <ryanlonergan@Ryans-MBP.fios-router.home>
2020-02-26 19:41:59 -0500

    Added basic functionality that pulls a sixers roster, create a new file, writes the player names and IDs to the file and then closes the file

--- process_cloud/teams_info.py
+++ process_cloud/teams_info.py
@@ -1,11 +1,23 @@
-from sportsreference.nba.teams import Teams
-from sportsreference.nba.rosters import Rosters
-from pprint import pprint
-import csv
-import os
+#!/usr/bin/env python3
+
+#from sportsreference.nba.teams import Teams
+from sportsreference.nba.roster import Roster
+#from pprint import pprint
+#import csv
+#import os
 
 #2013-2016
 def get_rosters(years):
     #years is a list of years to pull the rosters from
-    for year in years:
-        
+    with open("hinkie_roster.txt", "w") as f:
+    #    hinkie_roster = {}
+        for year in years:
+            #Gets roster for a given year
+            roster = Roster(team="PHI", year=str(year), slim=True).players
+            #incrementing through the player info and print playerID and name
+            for playerID, name in roster.items():
+                f.write("{}: PlayerID: {}\n".format(name, playerID))
+    f.close()
+
+
+get_rosters(["2018"])
